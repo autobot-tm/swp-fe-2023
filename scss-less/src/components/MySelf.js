daa6ff4ccb09f527357959f7f8d19b81b347003e
autobot-tm <autobot.tm19@gmail.com>
2023-03-03 12:10:23 +0700

    base

---
+++ scss-less/src/components/MySelf.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MySelf() {
+  return (
+    <>
+        <div className='container'>
+            <div className='row'> 
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MySelf
